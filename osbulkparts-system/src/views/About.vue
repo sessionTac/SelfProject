c30c4f2bf4dd6c0258f4d33c92bb39f940efd1b2
luka0317 <15063058059@qq.com>
2019-07-01 14:38:00 +0800

    11

--- osbulkparts-system/src/views/About.vue
+++ osbulkparts-system/src/views/About.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="about">
     <!--<img src="../../public/static/img/timg.gif" height="494" width="658"/>-->
-    <img src="/static/img/timg.gif" height="750" width="950"/>
+    <!--<img :src='a' height="750" width="950"/>-->
+    <img :src="'./static/img/timg.gif'" height="750" width="950"/>
   </div>
 </template>
+<script>
+  export default {
+    data (){
+      return{
+        // a:'./static/img/timg.gif',
+      }
+    }
+  }
+</script>
